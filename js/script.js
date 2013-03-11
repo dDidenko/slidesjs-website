e4b6f093b19eb2d2398c1a676d758e6d579eb1c4
Nathan Searles <nsearles@gmail.com>
2013-03-11 16:52:38 -0700

    Updated with SlidesJS 3 website

--- js/script.js
+++ js/script.js
@@ -1,50 +1,45 @@
 $(function() {
-     $('#slides').slides({
-        preload: true,
-		preloadImage: '/img/loading.gif',
-		play: 5000,
-		pause: 2500,
-		slideSpeed: 450,
-		hoverPause: true
-     });
-     
-     $('#main_nav').scrollspy();
+    $('#navbar').scrollspy()
 
-     $('#main_nav li.active').removeClass('active');
+    $('.nav a.scrollto').on('click', function(e) {
+        e.preventDefault();
+        $this = $(this);
+        offset = 60;
+        target = $this.attr('href');
+        targetPos = $(target).position().top;
+        window.scrollTo(0,targetPos - offset)
+    });
 
-     $('#main_nav li:first').addClass('active')
-     
-     $('#slidesjs').click(function(e) {
-         e.preventDefault();
-         window.scrollTo( 0, 0) ;
-     });
-    
-    $('.expand .show').click(function(e){
+    $(".brand").click(function(e) {
         e.preventDefault();
-    	var obj = $(this).parents('.expand');
-    	if ($(obj).hasClass('expanded')) {
-    		$(obj).removeClass('expanded');
-    		$('.content',obj).slideUp(100);					
-    		$(this).html('<i class="icon-plus-sign"></i> Show example');
-    	} else {
-    		$(obj).addClass('expanded');
-    		$('.content',obj).slideDown(150);
-    		$(this).html('<i class="icon-minus-sign"></i> Hide example');
-    	}
+        window.scrollTo( 0, 0) ;
     });
-    
-    $('.donate').click(function(e) {
+
+    $(".expand .show").click(function(e){
         e.preventDefault();
-        
-        $('#submit').click();
+        var obj = $(this).parents(".expand");
+        if ($(obj).hasClass("expanded")) {
+            $(obj).removeClass("expanded");
+            $(".content",obj).slideUp(100);
+            $(this).html("<i class='icon-plus-sign'></i> Show example");
+        } else {
+            $(obj).addClass("expanded");
+            $(".content",obj).slideDown(150);
+            $(this).html("<i class='icon-minus-sign'></i> Hide example");
+        }
     });
-    
-    $('.track_download').click(function(){
-		_gaq.push(['_trackEvent', 'Slides', 'Download', 'Slides GitHub Download']);
-	});
-	$('.track_donate').click(function(){
-		_gaq.push(['_trackEvent', 'Slides', 'Donate', 'Slides Donation']);
-	});
+
+    $(".collapse").collapse({
+        toggle: false
+    });
 
     Socialite.load();
+
+     $(".track_download").click(function(){
+        _gaq.push(["_trackEvent", "Slides", "Download", "Slides GitHub Download"]);
+    });
+
+    $(".track_donate").click(function(){
+        _gaq.push(["_trackEvent", "Slides", "Donate", "Slides Donation"]);
+    });
 });
